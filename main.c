10e9bb2f6221eb38901673aeb98cf3f8f3628d30
Myralllka <Olli25010@gmail.com>
2019-10-15 01:15:02 +0300

    add tests

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 //#include "libb/library.c"
-#include "libb/src/library.c"
+//#include "libb/src/library.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "tests/tests.c"
 
 int main(void) {
-    my_str_t x;
-    my_str_t y;
-    my_str_create(&x, 7);
-    my_str_create(&y, 5);
-    char *cst = "abcdefgaaaaaaaaaaaaa";
-    my_str_from_cstr(&x, cst, 0);
+//    my_str_t x;
+//    my_str_t y;
+//    my_str_create(&x, 7);
+//    my_str_create(&y, 5);
+//    char *cst = "abcdefgaaaaaaaaaaaaa";
+//    my_str_from_cstr(&x, cst, 0);
 //    puts("asdasd");
 //    printf("%s", my_str_get_cstr(&x));
 //    printf("%s\n", my_str_get_cstr(&x));
 //    printf("%s\n", my_str_get_cstr(&y));
-    char c[100];
-    my_str_substr_cstr(&x, c, 0, 5);
-    printf("%s", c);
-
-    my_str_free(&y);
-    my_str_free(&x);
+//    char c[100];
+//    my_str_substr_cstr(&x, c, 0, 5);
+//    printf("%s", c);
+    return tests();
+//    my_str_free(&y);
+//    my_str_free(&x);
 }
