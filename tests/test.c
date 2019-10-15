c9d7bddc04fc4e3e95d396a5030db6611fa5f829
Myralllka <Olli25010@gmail.com>
2019-10-15 14:19:00 +0300

    complete refactoring of libb to cstring

--- tests/test.c
+++ tests/test.c
@@ -3,16 +3,35 @@
 
 
 int tests(void) {
-    if (test_my_str_size()) return 1;
-    printf("complete test 1");
-    if (test_my_str_from_cstr()) return 2;
-    printf("complete test 2");
-    if (test_my_str_capacity()) return 3;
-    printf("complete test 3");
+    if (test_my_str_create()) printf("fail test 1\n");
+    if (test_my_str_from_cstr()) printf("fail test 2\n");
+    if (test_my_str_size()) printf("fail test 3\n");
+    if (test_my_str_capacity()) printf("fail test 4\n");
+    if (test_my_str_empty()) printf("fail test 5\n");
     return 0;
 }
 
 int test_my_str_create() {
+    my_str_t str;
+    my_str_create(&str, 1);
+    if (!my_str_create(NULL, 0)) return -1;
+    if (str.data == NULL) return -1;
+    my_str_free(&str);
+    return 0;
+}
+
+
+int test_my_str_from_cstr() {
+    my_str_t str;
+    my_str_create(&str, 0);
+    char *ccc = "hello my dear";
+    my_str_from_cstr(&str, ccc, 0);
+    for (int i = 0; i < str.size_m; i++) {
+        if (str.data[i] != ccc[i]) return 1;
+    }
+    if (str.size_m != 13) return 1;
+    if (str.capacity_m != str.size_m + 1) return 1;
+    my_str_free(&str);
     return 0;
 }
 
@@ -24,27 +43,32 @@ int test_my_str_size() {
     return 0;
 }
 
-int test_my_str_from_cstr() {
-    my_str_t str;
-    my_str_create(&str, 0);
-    char *ccc = "hello my dear";
-    my_str_from_cstr(&str, ccc, 0);
-    for (int i = 0; i < str.size_m; i++) {
-        if (str.data[i] != ccc[i]) return 1;
-    }
-    if (str.size_m != 13) return 1;
-    if (str.capacity_m != 27) return 1;
-    my_str_free(&str);
-    return 0;
-}
-
 int test_my_str_capacity() {
     my_str_t str;
     my_str_create(&str, 0);
     char *ccc = "hello";
     my_str_from_cstr(&str, ccc, 0);
-    if (str.capacity_m != 11) return 1;
-    if (str.data == NULL) return 1;
+    if (str.capacity_m != str.size_m + 1) return 1;
     my_str_free(&str);
+    return 0;
+}
+
+int test_my_str_empty() {
+        my_str_t str;
+        my_str_create(&str, 1);
+        my_str_from_cstr(&str, "hello", 0);
+        
+    return 0;
+}
+
+int test_my_str_getc() {
+    return 0;
+}
+
+int test_my_str_putc() {
+    return 0;
+}
+
+int test_my_str_get_cstr() {
     return 0;
 }
