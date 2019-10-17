1d5df4e278f5805c852d608a37c38bdf48e8bfcb
fenix-125 <yurijpasichnyk11@gmail.com>
2019-10-17 03:01:07 +0300

    - remake the test coling function; - add header and the screch for 'test_my_str_find'; - made some fixed or modifications in functions: 	my_str_find, my_str_cmp, my_str_cmp_cstr, my_str_find_c, 	my_str_find_if, my_str_write_file, my_str_read_file_delim, char_arr_len; - write documentation for some functions issue #15 ; - write tests issue #16 : 	test_my_str_read_file_delim, test_my_str_write_file, test_my_str_read_file, 	test_my_str_find_if, test_my_str_find_c, test_my_str_cmp_cstr, test_my_str_cmp;

--- lib/src/c_string.c
+++ lib/src/c_string.c
@@ -18,7 +18,7 @@ int my_str_create(my_str_t *str, size_t buf_size) {
         str->data[i] = 0;
     }
     return 0;
-};
+}
 
 void my_str_free(my_str_t *str) {
     // delete memory of my_str_t
@@ -31,7 +31,7 @@ int my_str_from_cstr(my_str_t *str, const char *cstr, size_t buf_size) {
     // if buf_size == 0 then buf_size will be equal to real size
     // return -1 if buf size is less then real size then it is a mistake
     // return -2 if it is impossible to give enough memory
-    //return 0 if everything is Ok
+    // return 0 if everything is Ok
     size_t real_cstring_size = 0;
     while (*(cstr + real_cstring_size) != '\0') {
         real_cstring_size += 1;
@@ -58,8 +58,8 @@ size_t my_str_size(const my_str_t *str) {
 }
 
 size_t my_str_capacity(const my_str_t *str) {
-    //Return buffer size (capacity) of the str
-    //0 for NULL pointer
+    // Return buffer size (capacity) of the str
+    // 0 for NULL pointer
     if (str == NULL) return 0;
     return str->capacity_m;
 }
@@ -71,7 +71,7 @@ int my_str_empty(const my_str_t *str) {
 }
 
 int my_str_getc(const my_str_t *str, size_t index) {
-    //return -1 if index is incorrect, otherwise the char on needed position
+    // return -1 if index is incorrect, otherwise the char on needed position
     if (str == NULL) return -1;
     if ((index > str->size_m - 1) || (index < 0)) return -1;
     return str->data[index];
@@ -93,10 +93,10 @@ const char *my_str_get_cstr(my_str_t *str) {
 }
 
 int my_str_pushback(my_str_t *str, char c) {
-    //Add a symbol (character) in the end of the my_str
-    //return 0 if there no mistakes
-    //return -1 if zero pointer
-    //return -2 if it is impossible to reserve new memory
+    // Add a symbol (character) in the end of the my_str
+    // return 0 if there no mistakes
+    // return -1 if zero pointer
+    // return -2 if it is impossible to reserve new memory
     if (str == NULL)
         return -1;
     if (my_str_reserve(str, str->size_m + 1) == -1)
@@ -107,10 +107,10 @@ int my_str_pushback(my_str_t *str, char c) {
 }
 
 int my_str_popback(my_str_t *str) {
-    //Delete symbol from the end
-    //return the symbol if there were no mistakes
-    //return -1 if zero pointer
-    //return -2 if my_str is empty
+    // Delete symbol from the end
+    // return the symbol if there were no mistakes
+    // return -1 if zero pointer
+    // return -2 if my_str is empty
     if (str == NULL)
         return -1;
     if (str->size_m == 0) {
@@ -121,10 +121,10 @@ int my_str_popback(my_str_t *str) {
 }
 
 int my_str_copy(const my_str_t *from, my_str_t *to, int reserve) {
-    //Copy the string
-    //If reserve is true, then buffer of new str is equal to previous one, otherwise minimum possible value
-    //return 0 if there is no mistakes
-    //return -1 if there no memory left
+    // Copy the string
+    // If reserve is true, then buffer of new str is equal to previous one, otherwise minimum possible value
+    // return 0 if there is no mistakes
+    // return -1 if there no memory left
     int new_buffer;
     if (reserve)
         new_buffer = from->capacity_m;
@@ -268,12 +268,11 @@ int my_str_shrink_to_fit(my_str_t *str) {
 
 // TODO: test my_str_find AND possibly delete input checks
 size_t my_str_find(const my_str_t *str, const my_str_t *tofind, size_t from) {
-    if (NULL == str || tofind == NULL || str->size_m < tofind->size_m || from >= str->size_m || from < 0 ||
-        tofind->size_m == 0)
-        return (size_t) (-2);
-//    if (tofind->size_m == 0)
-//        return (size_t)(-1);
-
+    // Find the 'tofind' string in the 'str' string starting to search from the 'from' index
+    // If the 'from' is closer ot the end of the 'str' than the length of the 'tofind'
+    // string than returns -1 (not found)
+    // If the function do not find the 'tofind' string then it returns -1
+    // else it return the index of the beginning of the searched substring in the 'str' string
     size_t i = from, match = 0;
     while (i < str->size_m && match < tofind->size_m) {
         if (str->data[i] == tofind->data[match])
@@ -289,44 +288,53 @@ size_t my_str_find(const my_str_t *str, const my_str_t *tofind, size_t from) {
 }
 
 int my_str_cmp(const my_str_t *str1, const my_str_t *str2) {
+    // This function return values that are as follows:
+    // Return value < 0 if str1 is alphabetical less than str2.
+    // Return value > 0 if str2 is alphabetical less than str1.
+    // Return value = 0 if str1 is equal to str2.
     size_t i = 0;
-    while (i < (*str1).size_m && i < (*str2).size_m) {
-        if ((*str1).data[i] > (*str2).data[i])
+    while (i < str1->size_m && i < str2->size_m) {
+        if (str1->data[i] > str2->data[i])
             return 1;
-        else if ((*str1).data[i] < (*str2).data[i])
+        else if (str1->data[i] < str2->data[i])
             return -1;
         i++;
     }
-    if ((*str1).size_m > (*str2).size_m)
+    if (str1->size_m > str2->size_m)
         return 1;
-    else if ((*str1).size_m < (*str2).size_m)
+    else if (str1->size_m < str2->size_m)
         return -1;
     return 0;
 }
 
-// TODO: test my_str_cmp_cstr
 int my_str_cmp_cstr(const my_str_t *str1, const char *cstr2) {
-    size_t str2_len = char_arr_len(cstr2);
-    if (str1->size_m > str2_len)
-        return 1;
-    else if (str1->size_m < str2_len)
-        return -1;
-    else {
-        for (int j = 0; j < str1->size_m; ++j) {
-            if (str1->data[j] != cstr2[j])
-                return -1;
-        }
-        return 0;
+    // This function return values that are as follows:
+    // Return value < 0 if str1 is alphabetical less than str2.
+    // Return value > 0 if str2 is alphabetical less than str1.
+    // Return value = 0 if str1 is equal to str2.
+    size_t i = 0;
+    size_t cstr2_len = char_arr_len(cstr2);
+    while (i < str1->size_m && i < cstr2_len) {
+        if (str1->data[i] > cstr2[i])
+            return 1;
+        else if (str1->data[i] < cstr2[i])
+            return -1;
+        i++;
     }
+    if ((*str1).size_m > cstr2_len)
+        return 1;
+    else if (str1->size_m < cstr2_len)
+        return -1;
+    return 0;
 }
 
-// TODO: test my_str_find_c
 size_t my_str_find_c(const my_str_t *str, char tofind, size_t from) {
+    // Find the tofind char in the string starting to search from the 'from' index
+    // If the from is larger than the size of string return -1 (not found)
+    // If the function do not find the char then it returns -1
+    // else it return the index of the char in the string
     if (from > str->size_m)
         return -1;
-    if (from < 0)
-        from = 0;
-//        exit(1);
 
     for (size_t j = from; j < str->size_m; ++j) {
         if (str->data[j] == tofind)
@@ -335,41 +343,62 @@ size_t my_str_find_c(const my_str_t *str, char tofind, size_t from) {
     return (size_t) (-1);
 }
 
-// TODO: test my_str_find_if
 size_t my_str_find_if(const my_str_t *str, int (*predicat)(int)) {
-    for (int j = 0; j < str->size_m; ++j) {
-        if (predicat(str->data[j]))
+    // Find the first occurrence of a char that satisfy the 'predicat' function
+    // If there is not such char in the string returns -1
+    for (size_t j = 0; j < str->size_m; ++j) {
+        if (predicat((int) str->data[j]))
             return j;
     }
     return (size_t) (-1);
 }
 
 int my_str_read_file(my_str_t *str, FILE *file) {
+    // read the file to the EOF
+    // return -1 if can not add a character from the file to the end of the string
+    // return 0 if everything is Ok
     return my_str_read_file_delim(str, file, EOF);
 }
 
 int my_str_read(my_str_t *str) {
+    // read the input from the standard input till the EOF
+    // return -1 if can not add a character from the file to the end of the string
+    // return 0 if everything is Ok
     return my_str_read_file(str, stdin);
 }
 
 int my_str_write_file(const my_str_t *str, FILE *file) {
+    // write the string to the file
+    // return -111 if can not create a new string
+    // return -211 if can not copy the string
+    // return the negative values of fprintf if there is an error
+    // return 0 if everything is Ok
     my_str_t printed_str;
     if (my_str_create(&printed_str, 0) != 0)
-        return -1;
+        return -111;
     if (my_str_copy(str, &printed_str, 1) != 0)
-        return -2;
-    fprintf(file, "%s", my_str_get_cstr(&printed_str));
+        return -211;
+    int res = fprintf(file, "%s", my_str_get_cstr(&printed_str));
+    if (!res) return res;
     return 0;
 }
 
 int my_str_write(const my_str_t *str) {
+    // write the string to the standard output file
+    // return -111 if can not create a new string
+    // return -211 if can not copy the string
+    // return the negative values of fprintf if there is an error
+    // return 0 if everything is Ok
     return my_str_write_file(str, stdout);
 }
 
 int my_str_read_file_delim(my_str_t *str, FILE *file, char delimiter) {
+    // read the file to the occurrence of the delimiter character
+    // return -1 if can not add a character from the file to the end of the string
+    // return 0 if everything is Ok
     int current_c;
     str->size_m = 0;
-    while ((current_c = fgetc(file)) != delimiter) {
+    while ((current_c = fgetc(file)) != delimiter && current_c != EOF) {
         if (my_str_pushback(str, (char) current_c) != 0)
             return -1;
     }
@@ -397,9 +426,8 @@ int my_str_realloc(my_str_t *str, size_t buffer) {
 }
 
 size_t char_arr_len(const char *s) {
+    // return the length of a char array
     size_t i = 0;
-    while (*(s + i) != '\0') {
-        i++;
-    }
+    while (s[i] != '\0') i++;
     return i;
 }
--- tests/test.c
+++ tests/test.c
@@ -3,36 +3,77 @@
 
 
 int tests(void) {
-    if (test_my_str_create()) printf("fail test 1\n");
-    if (test_my_str_from_cstr()) printf("fail test 2\n");
-    if (test_my_str_size()) printf("fail test 3\n");
-    if (test_my_str_capacity()) printf("fail test 4\n");
-    if (test_my_str_empty()) printf("fail test 5\n");
-    if (test_my_str_getc()) printf("fail test 6\n");
-    if (test_my_str_putc()) printf("fail test 7\n");
-    if (test_my_str_get_cstr()) printf("fail test 8\n");
-    if (test_my_str_pushback()) printf("fail test 9\n");
-    if (test_my_str_popback()) printf("fail test 10\n");
-    if (test_my_str_copy()) printf("fail test 11\n");
-    if (test_my_str_insert_c()) printf("fail test 13\n");
-    if (test_my_str_insert()) printf("fail test 14\n");
-    if (test_my_str_insert_cstr()) printf("fail test 15\n");
-//    if (test_my_str_append()) printf("fail test 16\n");
-//    if (test_my_str_append_cstr()) printf("fail test 17\n");
-//    if (test_my_str_substr()) printf("fail test 18\n");
-//    if (test_my_str_substr_cstr()) printf("fail test 19\n");
-//    if (test_my_str_reserve()) printf("fail test 20\n");
-//    if (test_my_str_shrink_to_fit()) printf("fail test 21\n");
-//    if (test_my_str_resize()) printf("fail test 22\n");
-//    if (test_my_str_cmp()) printf("fail test 23\n");
-//    if (test_my_str_cmp_cstr()) printf("fail test 24\n");
-//    if (test_my_str_find_c()) printf("fail test 25\n");
-//    if (test_my_str_find_if()) printf("fail test 26\n");
-//    if (test_my_str_read_file()) printf("fail test 27\n");
-//    if (test_my_str_read()) printf("fail test 28\n");
-//    if (test_my_str_write_file()) printf("fail test 29\n");
-//    if (test_my_str_write()) printf("fail test 30\n");
-//    if (test_my_str_read_file_delim()) printf("fail test 31\n");
+    char* names[] = {"test_my_str_create",         // 1
+                     "test_my_str_from_cstr",      // 2
+                     "test_my_str_size",           // 3
+                     "test_my_str_capacity",       // 4
+                     "test_my_str_empty",          // 5
+                     "test_my_str_getc",           // 6
+                     "test_my_str_putc",           // 7
+                     "test_my_str_get_cstr",       // 8
+                     "test_my_str_pushback",       // 9
+                     "test_my_str_popback",        // 10
+                     "test_my_str_copy",           // 11
+
+//                     "test_my_str_insert_c",       // 12
+//                     "test_my_str_insert",         // 13
+//                     "test_my_str_insert_cstr",    // 14
+//                     "test_my_str_append",         // 15
+//                     "test_my_str_append_cstr",    // 16
+//                     "test_my_str_substr",         // 17
+//                     "test_my_str_substr_cstr",    // 18
+//                     "test_my_str_reserve",        // 19
+//                     "test_my_str_shrink_to_fit",  // 20
+//                     "test_my_str_resize",         // 21
+//                     "test_my_str_find",           // 22
+
+                     "test_my_str_cmp",            // 15
+                     "test_my_str_cmp_cstr",       // 16
+                     "test_my_str_find_c",         // 17
+                     "test_my_str_find_if",
+                     "test_my_str_read_file",      // 18
+                     "test_my_str_write_file",     // 19
+                     "test_my_str_read_file_delim"};// 20
+
+    int (*functions[])(void) = {test_my_str_create,         // 1
+                                test_my_str_from_cstr,      // 2
+                                test_my_str_size,           // 3
+                                test_my_str_capacity,       // 4
+                                test_my_str_empty,          // 5
+                                test_my_str_getc,           // 6
+                                test_my_str_putc,           // 7
+                                test_my_str_get_cstr,       // 8
+                                test_my_str_pushback,       // 9
+                                test_my_str_popback,        // 10
+                                test_my_str_copy,           // 11
+
+//                                test_my_str_insert_c,       // 12
+//                                test_my_str_insert,         // 13
+//                                test_my_str_insert_cstr,    // 14
+//                                test_my_str_append,         // 15
+//                                test_my_str_append_cstr,    // 16
+//                                test_my_str_substr,         // 17
+//                                test_my_str_substr_cstr,    // 18
+//                                test_my_str_reserve,        // 19
+//                                test_my_str_shrink_to_fit,  // 20
+//                                test_my_str_resize,         // 21
+//                                test_my_str_find,           // 22
+
+                                test_my_str_cmp,            // 15
+                                test_my_str_cmp_cstr,       // 16
+                                test_my_str_find_c,         // 17
+                                test_my_str_find_if,
+                                test_my_str_read_file,      // 18
+                                test_my_str_write_file,     // 19
+                                test_my_str_read_file_delim};// 20
+    for (int i = 0; i < 18; ++i) {
+        int res = (functions[i])();
+        if (res)
+            printf("Test %i '%s':\t\tFail %i\n", i + 1, names[i], res);
+//        else
+//            printf("Test %i: OK\n", i + 1);
+    }
+
     return 0;
 }
 
@@ -170,7 +211,6 @@ int test_my_str_copy() {
 }
 
 int test_my_str_insert_c() {
-
     return -1;
 }
 
@@ -210,38 +250,190 @@ int test_my_str_resize() {
     return -1;
 }
 
-int test_my_str_cmp() {
+int test_my_str_find() {
     return -1;
+}
+
+int test_my_str_cmp() {
+    my_str_t str;
+    my_str_t tmp;
+    if (my_str_create(&str, 1) != 0) return -12;
+    if (my_str_create(&tmp, 1) != 0) return -12;
+    if (my_str_from_cstr(&str, "abcd", 0) != 0) return -13;
+
+    if (my_str_from_cstr(&tmp, "a", 0) != 0) return -13;
+    if (my_str_cmp(&str, &tmp) != 1) return -1;
+
+    if (my_str_from_cstr(&tmp, "bcscas", 0) != 0) return -13;
+    if (my_str_cmp(&str, &tmp) != -1) return -2;
+
+    if (my_str_from_cstr(&tmp, "c", 0) != 0) return -13;
+    if (my_str_cmp(&str, &tmp) != -1) return -3;
+
+    if (my_str_from_cstr(&str, "klmnop", 0) != 0) return -13;
+    if (my_str_from_cstr(&tmp, "asdasd", 0) != 0) return -13;
+    if (my_str_cmp(&str, &tmp) != 1) return -4;
+
+    if (my_str_from_cstr(&tmp, "b", 0) != 0) return -13;
+    if (my_str_cmp(&str, &tmp) != 1) return -5;
+
+    if (my_str_from_cstr(&str, "gfg", 0) != 0) return -13;
+    if (my_str_from_cstr(&tmp, "gbasd", 0) != 0) return -13;
+    if (my_str_cmp(&str, &tmp) != 1) return -6;
+
+    if (my_str_from_cstr(&tmp, "gfg", 0) != 0) return -13;
+    if (my_str_cmp(&str, &tmp) != 0) return -7;
+
+    if (my_str_from_cstr(&str, "v", 0) != 0) return -13;
+    if (my_str_from_cstr(&tmp, "v", 0) != 0) return -13;
+    if (my_str_cmp(&str, &tmp) != 0) return -8;
+
+    my_str_free(&str);
+    return 0;
 }
 
 int test_my_str_cmp_cstr() {
-    return -1;
+    my_str_t str;
+
+    if (my_str_create(&str, 1) != 0) return -12;
+    if (my_str_from_cstr(&str, "abcd", 0) != 0) return -13;
+
+    if (my_str_cmp_cstr(&str, "a") != 1) return -1;
+
+    if (my_str_cmp_cstr(&str, "bcscas") != -1) return -2;
+
+    if (my_str_cmp_cstr(&str, "c") != -1) return -3;
+
+    if (my_str_from_cstr(&str, "klmnop", 0) != 0) return -13;
+    if (my_str_cmp_cstr(&str, "asdasd") != 1) return -4;
+
+    if (my_str_cmp_cstr(&str, "b") != 1) return -5;
+
+    if (my_str_from_cstr(&str, "gfg", 0) != 0) return -13;
+    if (my_str_cmp_cstr(&str, "gbasd") != 1) return -6;
+
+    if (my_str_cmp_cstr(&str, "gfg") != 0) return -7;
+
+    if (my_str_from_cstr(&str, "v", 0) != 0) return -13;
+    if (my_str_cmp_cstr(&str, "v") != 0) return -8;
+
+    my_str_free(&str);
+    return 0;
 }
 
 int test_my_str_find_c() {
-    return -1;
+    my_str_t str;
+    if (my_str_create(&str, 1) != 0) return -12;
+    if (my_str_from_cstr(&str, "the best test Ever, for your good mood! :)\n", 0) != 0) return -13;
+
+    if (my_str_find_c(&str, 't', 0) != 0) return -2;
+    if (my_str_find_c(&str, 't', 1) != 7) return -3;
+    if (my_str_find_c(&str, 't', 9) != 9) return -4;
+    if (my_str_find_c(&str, 't', 10) != 12) return -5;
+    if (my_str_find_c(&str, 't', 13) != -1) return -6;
+    if (my_str_find_c(&str, 't', 100) != -1) return -7;
+
+    if (my_str_find_c(&str, 'e', 0) != 2) return -8;
+    if (my_str_find_c(&str, 'e', 14) != 16) return -9;
+    if (my_str_find_c(&str, 'e', 15) != 16) return -10;
+    if (my_str_find_c(&str, '\n', 15) != 42) return -11;
+
+    my_str_free(&str);
+    return 0;
+}
+
+int false_pred(int c) {
+    return 0;
+}
+
+int five_mod_pred(int c) {
+    switch ((char) c) {
+        case 'a':
+            return 1;
+        case 'b':
+            return 1;
+        case 'c':
+            return 1;
+    }
+    return 0;
 }
 
 int test_my_str_find_if() {
-    return -1;
+    my_str_t str;
+    if (my_str_create(&str, 1) != 0) return -12;
+    if (my_str_from_cstr(&str, "the best test Ever, for your good mood! :)\n", 0) != 0) return -13;
+
+    if (my_str_find_if(&str, five_mod_pred) != (size_t) (4)) return -1;
+    if (my_str_find_if(&str, false_pred) != (size_t) (-1)) return -2;
+
+    my_str_free(&str);
+    return 0;
 }
 
 int test_my_str_read_file() {
-    return -1;
-}
+    // read the file docs/alice29.txt and checks the number of characters that should be 152089
+    my_str_t str;
+    FILE* alice_p = fopen("../docs/alice29.txt", "r");
 
-int test_my_str_read() {
-    return -1;
+    if (alice_p == NULL) return -11;
+    if (my_str_create(&str, 1) != 0) return -12;
+
+//    delimiter is not in file
+    if (my_str_read_file(&str, alice_p) != 0) return -1;
+    if (str.size_m != 152089) return -2;
+
+    my_str_free(&str);
+    fclose(alice_p);
+    return 0;
 }
 
 int test_my_str_write_file() {
-    return -1;
-}
+    // create file "docs/test_out.txt" with content "test_my_str_write_file\n"
+    my_str_t str;
+    FILE* output_p = fopen("../docs/test_out.txt", "w");
 
-int test_my_str_write() {
-    return -1;
+    if (output_p == NULL) return -11;
+    if (my_str_create(&str, 1) != 0) return -12;
+    if (my_str_from_cstr(&str, "test_my_str_write_file\n", 0) != 0) return -13;
+    if (my_str_write_file(&str, output_p) != 0) return -1;
+
+    my_str_free(&str);
+    fclose(output_p);
+    return 0;
 }
 
 int test_my_str_read_file_delim() {
-    return -1;
+    my_str_t str;
+    FILE* alice_p = fopen("../docs/alice29.txt", "r");
+
+    if (alice_p == NULL) return -11;
+    if (my_str_create(&str, 1) != 0) return -12;
+
+//    delimiter on the very beginning
+    if (my_str_read_file_delim(&str, alice_p, '\n') != 0) return -1;
+    if (str.size_m != 0) return -2; // !!! somehow  str.size_m = 1
+    rewind(alice_p);  // reset the cursor of the file
+
+    // debug lines
+//    printf("res: %i\n", my_str_read_file_delim(&str, alice_p, 'E'));
+//    printf("res: %zu\n", str.size_m);
+//    printf("%c", str.data[0]);
+//    for (size_t i = 0; i <= str.size_m; ++i) {
+//        printf("%zu%c", i, str.data[i]);
+//    }
+//    printf("\n");
+//    rewind(alice_p);
+
+//    delimiter on the firs page
+    if (my_str_read_file_delim(&str, alice_p, 'E') != 0) return -3;
+    if (str.size_m != 24) return -4;
+    rewind(alice_p);
+//    delimiter is not in file
+    if (my_str_read_file_delim(&str, alice_p, '{') != 0) return -5;
+    if (str.size_m != 152089) return -6;
+    rewind(alice_p);
+
+    my_str_free(&str);
+    fclose(alice_p);
+    return 0;
 }
